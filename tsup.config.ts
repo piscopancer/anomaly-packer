39addbc9a33bc6ecc0b3ecd7a058d72f558b50b7
piscopancer <igor.bistr01092003@gmail.com>
2024-08-04 14:13:02 +0300

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,9 @@ import * as path from 'path'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  // clean: true,
   entry: ['src/index.ts', 'src/cli/anomaly-packer.ts'],
   dts: true,
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   onSuccess() {
     return fs.cp(path.join(__dirname, 'src/types'), path.join(__dirname, 'dist/types'), { recursive: true })
   },
